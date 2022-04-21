3a10accc6db58d316304a2f4836d358ba194cc80
dmissaelpb <dmissaelpb@hotmail.com>
2022-04-20 23:55:22 -0500

    primer commit creacion de proyecto

---
+++ src/main/java/com/app2/app12/App12Application.java
@@ -0,0 +1,13 @@
+package com.app2.app12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App12Application.class, args);
+	}
+
+}
